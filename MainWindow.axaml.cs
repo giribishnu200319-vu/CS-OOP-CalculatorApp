f95fab1e9799138e5887ce456742197047ef8447
Bishnu98084 <bishnugiri5423@gmail.com>
2026-05-30 08:30:16 +0300

    Added the GUI controller and function

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,11 +1,40 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System.Data;
 
-namespace Test;
+namespace CalculatorApp;
 
 public partial class MainWindow : Window
 {
     public MainWindow()
     {
         InitializeComponent();
+    }
+    private void Button_Click(object? sender, RoutedEventArgs e)
+    {
+        var button = sender as Button;
+
+        if (button != null)
+        {
+            DisplayBox.Text += button.Content?.ToString();
+        }
+    }
+
+    private void Clear_Click(object? sender, RoutedEventArgs e)
+    {
+        DisplayBox.Text = "";
+    }
+
+    private void Equals_Click(object? sender, RoutedEventArgs e)
+    {
+        try
+        {
+            var result = new DataTable().Compute(DisplayBox.Text, null);
+            DisplayBox.Text = result.ToString();
+        }
+        catch
+        {
+            DisplayBox.Text = "Error";
+        }
     }
 }
